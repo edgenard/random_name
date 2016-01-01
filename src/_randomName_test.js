d36f08a595e21ad91a15d3a624f2fb1fa8636707
Emmanuel Genard <edgenard@gmail.com>
2016-01-01 15:10:49 -0500

    Refactored helper function to accept argument

--- src/_randomName_test.js
+++ src/_randomName_test.js
@@ -1,3 +1,4 @@
+/* globals KeyBoardEvent: false */
 (function() {
   'use strict';
   var assert = require("./assert");
@@ -68,12 +69,73 @@
       assert.equal(secondChild, "BUTTON");
       assert.equal(thirdChild, "BUTTON");
     });
+
+    it("the two other buttons have the write inner text", function () {
+      var element = document.createElement("div");
+      var mouseClick = setupMouseClick();
+      randomName.initialize(element);
+      var button = element.firstChild;
+
+      button.dispatchEvent(mouseClick);
+      var secondChild = element.children[1].innerHTML;
+      var thirdChild = element.children[2].innerHTML;
+
+      assert.equal(secondChild, "Add more names");
+      assert.equal(thirdChild, "Finished adding names");
+    });
+
+    it("input box listens for return key", function () {
+      var element = document.createElement("div");
+      var mouseClick = setupMouseClick();
+      randomName.initialize(element);
+      var button = element.firstChild;
+
+      button.dispatchEvent(mouseClick);
+      var input = element.firstChild;
+      var enter = setupKeyPress("Enter");
+
+
+      assert.isFalse(input.dispatchEvent(enter));
+    });
+
+    it.skip("input box doesn't affect other keys", function () {
+      var element = document.createElement("div");
+      var mouseClick = setupMouseClick();
+      randomName.initialize(element);
+      var button = element.firstChild;
+
+      button.dispatchEvent(mouseClick);
+      var input = element.firstChild;
+
+    });
+
+    //TODO:
+    //1. Add event listeners on input box and other buttons
+    //2. Write functionality of eventHandlers.
+    describe("Adding Names", function () {
+
+    });
   });
 
   function setupMouseClick(){
     var mouseClick = document.createEvent("MouseEvent");
     mouseClick.initEvent("click", false, true);
     return mouseClick;
+  }
+
+  function setupKeyPress(keypressed) {
+    var keyboardEvent;
+    if(typeof window.KeyboardEvent !== "function"){
+      keyboardEvent = document.createEvent("KeyboardEvent");
+      keyboardEvent.initKeyboardEvent("keypress", false, true, window, keypressed, 0, "", false, "");
+    }else {
+      keyboardEvent = new KeyboardEvent('keypress', {
+        key: keypressed,
+        keyIdentifier: keypressed,
+        cancelable: true
+      });
+    }
+    return keyboardEvent;
   }
 
 }());
--- src/randomName.js
+++ src/randomName.js
@@ -5,18 +5,26 @@
       element.appendChild(button);
       button.innerHTML = "Click to add names";
       element.classList.add("random-name-picker");
-      button.addEventListener("click", addNames);
+      button.addEventListener("click", startAddingNames);
     }
 
-    function addNames(event) {
+    function startAddingNames(event) {
       event.preventDefault();
       var parent = event.currentTarget.parentElement;
       var input = document.createElement("input");
       input.setAttribute("type", "text");
+      input.addEventListener("keypress", function (event) {
+        console.log(event.keyCode, event.key, event.keyIdentifier);
+        if (event.key === "Enter" || event.keyIdentifier === "Enter"){
+          event.preventDefault();  
+        }
+      });
       parent.removeChild(parent.firstChild);
       parent.appendChild(input);
       var moreButton = document.createElement("button");
       var closeButton = document.createElement("button");
+      moreButton.innerHTML = "Add more names";
+      closeButton.innerHTML = "Finished adding names";
       parent.appendChild(moreButton);
       parent.appendChild(closeButton);
     }
